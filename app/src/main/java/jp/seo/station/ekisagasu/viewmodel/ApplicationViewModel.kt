a3cda6fb88a5106d4e670d49a555749f8096fbe4
senda_k <s.kaoru509@gmail.com>
2022-07-03 23:58:16 +0900

    削除：ApplicationViewModel 不要な機能

--- app/src/main/java/jp/seo/station/ekisagasu/viewmodel/ApplicationViewModel.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/viewmodel/ApplicationViewModel.kt
@@ -1,25 +1,17 @@
 package jp.seo.station.ekisagasu.viewmodel
 
 import android.content.Context
-import android.location.Location
 import androidx.annotation.MainThread
 import androidx.lifecycle.*
 import jp.seo.station.ekisagasu.Line
-import jp.seo.station.ekisagasu.Station
 import jp.seo.station.ekisagasu.core.NavigationRepository
-import jp.seo.station.ekisagasu.core.PrefectureRepository
 import jp.seo.station.ekisagasu.core.StationRepository
 import jp.seo.station.ekisagasu.core.UserRepository
-import jp.seo.station.ekisagasu.model.AppMessage
 import jp.seo.station.ekisagasu.repository.AppLogger
 import jp.seo.station.ekisagasu.repository.LocationRepository
-import jp.seo.station.ekisagasu.utils.UnitLiveEvent
 import jp.seo.station.ekisagasu.utils.combineLiveData
 import jp.seo.station.ekisagasu.utils.getViewModelFactory
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.io.PrintWriter
-import java.io.StringWriter
 
 /**
  * ApplicationComponentでInjectするViewModel(SingletonScoped)
@@ -51,24 +43,6 @@ class ApplicationViewModel(
                 ApplicationViewModel::class.java
             )
         }
-    }
-
-    var isActivityAlive = false
-    var isServiceAlive = false
-
-    val requestFinishActivity = UnitLiveEvent(true)
-    val requestFinishService = UnitLiveEvent(true)
-
-    @MainThread
-    fun finish() {
-        setSearchState(false)
-        requestFinishActivity.call()
-        requestFinishService.call()
-
-        // this view model is in application-scoped
-        // clear variable which is needed to be initialized when activity rebooted
-        fixTimer.value = false
-        nightMode.value = false
     }
 
     private var hasAppReboot = false
@@ -134,10 +108,6 @@ class ApplicationViewModel(
         }
     }
 
-    val startTimer = UnitLiveEvent(false)
-    val fixTimer = MutableLiveData(false)
-    val nightMode = MutableLiveData(false)
-
     val isNavigationRunning = navigator.running
 
     fun setNavigationLine(line: Line?) {
@@ -157,13 +127,6 @@ class ApplicationViewModel(
         }
     }
 
-    fun onServiceInit(context: Context, prefectureRepository: PrefectureRepository) {
-        viewModelScope.launch(Dispatchers.IO) {
-            //userRepository.onAppReboot(context)
-            prefectureRepository.setData(context)
-        }
-    }
-
     val appMessage = logger.message
 
     fun message(text: String) = viewModelScope.launch {
@@ -173,51 +136,4 @@ class ApplicationViewModel(
     fun error(text: String) = viewModelScope.launch {
         logger.error(text, text)
     }
-
-    fun saveMessage(message: AppMessage) =
-        viewModelScope.launch(Dispatchers.IO) {
-            when (message) {
-                is AppMessage.AppLog -> userRepository.logMessage(message.message)
-                is AppMessage.AppError -> {
-                    val str = if (message.cause == null) {
-                        message.message
-                    } else {
-                        val sw = StringWriter()
-                        val pw = PrintWriter(sw)
-                        message.cause.printStackTrace(pw)
-                        String.format("%s caused by;\n%s", message.message, sw.toString())
-                    }
-                    userRepository.logError(str)
-                }
-                is AppMessage.AppResolvableException -> userRepository.logError(message.exception.toString())
-            }
-        }
-
-    fun updateLocation(location: Location) = viewModelScope.launch {
-        userRepository.logLocation(location.latitude, location.longitude)
-        stationRepository.updateNearestStations(location)
-        stationRepository.nearestStation.value?.let {
-            navigator.updateLocation(location, it.station)
-        }
-    }
-
-    fun setSearchK(k: Int) = viewModelScope.launch {
-        stationRepository.setSearchK(k)
-    }
-
-    fun setSearchInterval(sec: Int) {
-        if (isRunning.value == true) {
-            gps.startWatchCurrentLocation(sec)
-        }
-    }
-
-    fun logStation(station: Station) = viewModelScope.launch {
-        userRepository.logStation(String.format("%s(%d)", station.name, station.code))
-    }
-
-
-    fun onServiceFinish(context: Context) = viewModelScope.launch {
-        userRepository.onAppFinish(context)
-    }
-
 }
