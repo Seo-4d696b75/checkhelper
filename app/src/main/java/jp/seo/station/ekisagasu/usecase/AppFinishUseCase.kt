10182a7e5e9ae70d3ef3fc9566fa23ce73273361
senda_k <s.kaoru509@gmail.com>
2022-07-07 22:47:24 +0900

    修正：細かな修正

--- app/src/main/java/jp/seo/station/ekisagasu/usecase/AppFinishUseCase.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/usecase/AppFinishUseCase.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
 import jp.seo.station.ekisagasu.core.UserRepository
 import jp.seo.station.ekisagasu.repository.AppStateRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 class AppFinishUseCase @Inject constructor(
     @ApplicationContext private val context: Context,
     private val userRepository: UserRepository,
