7d2660f342f0534fed8e26a2b26e35aa67424815
senda_k <s.kaoru509@gmail.com>
2022-07-09 01:53:00 +0900

    修正：BootUseCase

--- app/src/main/java/jp/seo/station/ekisagasu/usecase/BootUseCase.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/usecase/BootUseCase.kt
@@ -3,19 +3,22 @@ package jp.seo.station.ekisagasu.usecase
 import android.content.Context
 import dagger.hilt.android.qualifiers.ApplicationContext
 import jp.seo.station.ekisagasu.core.PrefectureRepository
-import jp.seo.station.ekisagasu.core.UserRepository
+import jp.seo.station.ekisagasu.repository.LogRepository
+import jp.seo.station.ekisagasu.repository.UserSettingRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class BootUseCase @Inject constructor(
     @ApplicationContext private val context: Context,
-    private val userRepository: UserRepository,
+    private val logRepository: LogRepository,
+    private val settingRepository: UserSettingRepository,
     private val prefectureRepository: PrefectureRepository,
 ) {
 
     suspend operator fun invoke() = withContext(Dispatchers.IO) {
-        userRepository.onAppReboot(context)
+        logRepository.onAppBoot(context)
+        settingRepository.load()
         prefectureRepository.setData(context)
     }
 }
