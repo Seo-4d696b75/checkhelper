0bbedf6c4171b246b9bb283c6559615665a65116
senda_k <s.kaoru509@gmail.com>
2022-07-09 22:20:09 +0900

    修正：StationFragment ViewModel追加

---
+++ app/src/main/java/jp/seo/station/ekisagasu/ui/station/StationViewModel.kt
@@ -0,0 +1,9 @@
+package jp.seo.station.ekisagasu.ui.station
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StationViewModel @Inject constructor() : ViewModel() {
+}
