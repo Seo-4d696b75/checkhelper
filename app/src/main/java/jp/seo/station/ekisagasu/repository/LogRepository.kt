f69c5c68faead20a1deca0fc9f6433f58c515837
senda_k <s.kaoru509@gmail.com>
2022-07-14 01:01:16 +0900

    修正：AppLoggerファイル分割

--- app/src/main/java/jp/seo/station/ekisagasu/repository/LogRepository.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/repository/LogRepository.kt
@@ -1,15 +1,11 @@
 package jp.seo.station.ekisagasu.repository
 
 import android.content.Context
-import com.google.android.gms.common.api.ResolvableApiException
-import jp.seo.station.ekisagasu.core.AppLog
-import jp.seo.station.ekisagasu.core.AppRebootLog
+import jp.seo.station.ekisagasu.database.AppLog
+import jp.seo.station.ekisagasu.database.AppRebootLog
 import jp.seo.station.ekisagasu.model.AppMessage
 import jp.seo.station.ekisagasu.model.LogTarget
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 interface LogRepository {
     suspend fun saveMessage(message: AppMessage)
@@ -19,29 +15,4 @@ interface LogRepository {
     val logs: StateFlow<List<AppLog>>
     suspend fun onAppBoot(context: Context)
     suspend fun onAppFinish(context: Context)
-}
-
-interface AppLogger {
-    fun CoroutineScope.log(message: String)
-    fun CoroutineScope.error(message: String, cause: Throwable? = null)
-    fun CoroutineScope.requestExceptionResolved(message: String, e: ResolvableApiException)
-}
-
-class AppLoggerImpl @Inject constructor(
-    private val repository: AppStateRepository
-) : AppLogger {
-    override fun CoroutineScope.log(message: String) {
-        launch { repository.emitMessage(AppMessage.Log(message)) }
-    }
-
-    override fun CoroutineScope.error(message: String, cause: Throwable?) {
-        launch { repository.emitMessage(AppMessage.Error(message, cause)) }
-    }
-
-    override fun CoroutineScope.requestExceptionResolved(
-        message: String,
-        e: ResolvableApiException
-    ) {
-        launch { repository.emitMessage(AppMessage.ResolvableException(message, e)) }
-    }
 }
