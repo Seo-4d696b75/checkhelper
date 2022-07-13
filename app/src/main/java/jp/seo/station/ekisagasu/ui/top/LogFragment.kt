474a3175f13e95b71013cb2ccc5377b7a0516daa
senda_k <s.kaoru509@gmail.com>
2022-07-14 00:35:37 +0900

    修正：LogFragment

---
+++ app/src/main/java/jp/seo/station/ekisagasu/ui/top/LogFragment.kt
@@ -0,0 +1,171 @@
+package jp.seo.station.ekisagasu.ui.top
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import dagger.hilt.android.AndroidEntryPoint
+import jp.seo.station.ekisagasu.R
+import jp.seo.station.ekisagasu.core.AppLog
+import jp.seo.station.ekisagasu.databinding.FragmentLogBinding
+import jp.seo.station.ekisagasu.ui.dialog.AppHistoryDialogDirections
+import jp.seo.station.ekisagasu.utils.TIME_PATTERN_MILLI_SEC
+import jp.seo.station.ekisagasu.utils.formatTime
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+/**
+ * @author Seo-4d696b75
+ * @version 2020/12/21.
+ */
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class LogFragment : Fragment() {
+
+    private val viewModel: LogViewModel by viewModels()
+
+    private lateinit var binding: FragmentLogBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.fragment_log,
+            container,
+            false,
+        )
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val context = requireContext()
+        binding.listLog.apply {
+            addItemDecoration(
+                DividerItemDecoration(
+                    context,
+                    LinearLayoutManager.VERTICAL,
+                )
+            )
+        }
+
+        binding.spinnerFilterLog.also {
+            val adapter = LogTypeAdapter(context, LogFilter.all)
+            it.adapter = adapter
+            it.setOnItemClickListener { _, _, position, _ ->
+                val filter = adapter.getItem(position)!!
+                viewModel.setLogFilter(filter)
+            }
+        }
+
+        binding.listLog.also {
+            val adapter = LogAdapter(context, emptyList())
+            it.layoutManager =
+                LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false).apply {
+                    stackFromEnd = true
+                }
+            it.adapter = adapter
+            viewModel.logs
+                .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                .onEach {
+                    adapter.logs = it
+                    adapter.notifyDataSetChanged()
+                }
+                .launchIn(viewLifecycleOwner.lifecycleScope)
+        }
+
+        binding.buttonWriteLog.setOnClickListener {
+            viewModel.requestWriteLog(getString(R.string.app_name))
+        }
+
+        binding.buttonSelectHistory.setOnClickListener {
+            val action = AppHistoryDialogDirections.actionGlobalAppHistoryDialog()
+            findNavController().navigate(action)
+        }
+
+        viewModel.onLogFileUriResolved
+            .flowWithLifecycle(viewLifecycleOwner.lifecycle)
+            .onEach {
+                viewModel.writeLog(it, context.contentResolver)
+            }
+            .launchIn(viewLifecycleOwner.lifecycleScope)
+    }
+
+}
+
+class LogTypeAdapter(
+    context: Context,
+    values: Array<LogFilter>
+) : ArrayAdapter<LogFilter>(context, 0, values) {
+
+    private val inflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val value = getItem(position)
+        val view =
+            convertView ?: inflater.inflate(android.R.layout.simple_spinner_item, null, false)
+        if (value != null && view is TextView) {
+            view.text = value.name
+        }
+        return view
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val value = getItem(position)
+        val view = convertView ?: inflater.inflate(
+            android.R.layout.simple_spinner_dropdown_item,
+            null,
+            false
+        )
+        if (value != null && view is TextView) {
+            view.text = value.name
+        }
+        return view
+    }
+
+}
+
+class LogViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val time: TextView = view.findViewById(R.id.text_log_time)
+    val message: TextView = view.findViewById(R.id.text_log_message)
+}
+
+class LogAdapter(context: Context, var logs: List<AppLog>) :
+    RecyclerView.Adapter<LogViewHolder>() {
+
+    private val inflater = LayoutInflater.from(context)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LogViewHolder {
+        val view = inflater.inflate(R.layout.cell_list_log, parent, false)
+        return LogViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: LogViewHolder, position: Int) {
+        val log = logs[position]
+        holder.time.text = formatTime(TIME_PATTERN_MILLI_SEC, log.timestamp)
+        holder.message.text = log.message
+    }
+
+    override fun getItemCount(): Int {
+        return logs.size
+    }
+
+}
---
+++ app/src/main/java/jp/seo/station/ekisagasu/ui/top/LogViewModel.kt
@@ -0,0 +1,122 @@
+package jp.seo.station.ekisagasu.ui.top
+
+import android.app.Activity
+import android.content.ContentResolver
+import android.content.Intent
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.seo.station.ekisagasu.core.AppLog
+import jp.seo.station.ekisagasu.model.AppMessage
+import jp.seo.station.ekisagasu.repository.AppStateRepository
+import jp.seo.station.ekisagasu.repository.LogRepository
+import jp.seo.station.ekisagasu.utils.TIME_PATTERN_DATETIME
+import jp.seo.station.ekisagasu.utils.TIME_PATTERN_DATETIME_FILE
+import jp.seo.station.ekisagasu.utils.TIME_PATTERN_MILLI_SEC
+import jp.seo.station.ekisagasu.utils.formatTime
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import java.io.BufferedWriter
+import java.io.IOException
+import java.io.OutputStreamWriter
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class LogViewModel @Inject constructor(
+    logRepository: LogRepository,
+    private val appStateRepository: AppStateRepository,
+) : ViewModel() {
+    val target = logRepository.logFilter
+
+    private val _filter = MutableStateFlow(LogFilter(AppLog.FILTER_ALL, "ALL"))
+
+    fun setLogFilter(filter: LogFilter) {
+        _filter.value = filter
+    }
+
+    val logs: StateFlow<List<AppLog>> = combine(
+        logRepository.logs,
+        _filter,
+    ) { logs, filter ->
+        logs.filter { (it.type and filter.filter) > 0 }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+
+    fun requestWriteLog(title: String) = viewModelScope.launch {
+        val builder = StringBuilder()
+        val time = Date()
+        val type = _filter.value
+        val list = logs.value
+
+        val fileName = String.format(
+            Locale.US, "%s_%sLog_%s.txt", title, type.name, formatTime(
+                TIME_PATTERN_DATETIME_FILE, time
+            )
+        )
+        builder.append(title)
+        builder.append("\nlog type : ")
+        builder.append(type.name)
+        builder.append("\nwritten time : ")
+        builder.append(formatTime(TIME_PATTERN_DATETIME, time))
+        for (log in list) {
+            builder.append("\n")
+            builder.append(formatTime(TIME_PATTERN_MILLI_SEC, log.timestamp))
+            builder.append(" ")
+            builder.append(log.message)
+        }
+        fileContext = builder.toString()
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            this.type = "text/plain"
+            putExtra(Intent.EXTRA_TITLE, fileName)
+        }
+        // TODO activityにcontent resolverをgetしてもらう
+        appStateRepository.emitMessage(
+            AppMessage.StartActivityForResult(REQUEST_CODE, intent)
+        )
+    }
+
+    private var fileContext: String? = null
+
+    val onLogFileUriResolved = appStateRepository.message
+        .filterIsInstance<AppMessage.ReceiveActivityResult>()
+        .filter {
+            it.code == REQUEST_CODE && it.result == Activity.RESULT_OK
+        }.map {
+            it.data?.data
+        }.filterNotNull()
+
+    fun writeLog(uri: Uri, resolver: ContentResolver) = viewModelScope.launch(Dispatchers.IO) {
+        val str = fileContext ?: return@launch
+        try {
+            resolver.openOutputStream(uri).use {
+                val writer = BufferedWriter(OutputStreamWriter(it, Charsets.UTF_8))
+                writer.write(str)
+                writer.close()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        fileContext = null
+    }
+
+    companion object {
+        const val REQUEST_CODE = 20220713
+    }
+}
+
+data class LogFilter(
+    val filter: Int,
+    val name: String,
+) {
+    companion object {
+        val all = arrayOf(
+            LogFilter(AppLog.FILTER_ALL, "ALL"),
+            LogFilter(AppLog.TYPE_SYSTEM, "System"),
+            LogFilter(AppLog.FILTER_GEO, "Geo"),
+            LogFilter(AppLog.TYPE_STATION, "Station")
+        )
+    }
+}
