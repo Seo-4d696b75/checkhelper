5beadfd031d93256f3a7a4fc3018cecac9d55fc2
senda_k <s.kaoru509@gmail.com>
2022-07-31 17:34:58 +0900

    修正：DownloadClient実装

--- app/src/main/java/jp/seo/station/ekisagasu/usecase/DataUpdateUseCase.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/usecase/DataUpdateUseCase.kt
@@ -1,22 +1,24 @@
 package jp.seo.station.ekisagasu.usecase
 
-import jp.seo.station.ekisagasu.api.getDownloadClient
+import jp.seo.station.ekisagasu.api.DownloadClient
 import jp.seo.station.ekisagasu.database.StationDao
 import jp.seo.station.ekisagasu.model.DataLatestInfo
 import jp.seo.station.ekisagasu.model.DataUpdateProgress
+import jp.seo.station.ekisagasu.model.StationData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import javax.inject.Inject
-import kotlin.math.floor
 
 @ExperimentalSerializationApi
 class DataUpdateUseCase @Inject constructor(
     private val dao: StationDao,
+    private val downloadClient: DownloadClient,
     private val json: Json,
 ) {
 
@@ -28,14 +30,17 @@ class DataUpdateUseCase @Inject constructor(
             _progress.emit(DataUpdateProgress.Download(0))
             try {
                 var percent = 0
-                val download = getDownloadClient(json) { length: Long ->
-                    val p = floor(length.toFloat() / info.length * 100.0f).toInt()
+                val data = downloadClient(info.url) {
+                    val p = (it * 100 / info.length).toInt()
                     if (p in 1..100 && p > percent) {
-                        launch(Dispatchers.Main) { _progress.emit(DataUpdateProgress.Download(p)) }
+                        launch(Dispatchers.Main) {
+                            _progress.emit(DataUpdateProgress.Download(p))
+                        }
                         percent = p
                     }
+                }.let { str ->
+                    json.decodeFromString<StationData>(str)
                 }
-                val data = download.getData(info.url)
                 withContext(Dispatchers.Main) {
                     _progress.emit(DataUpdateProgress.Save)
                 }
