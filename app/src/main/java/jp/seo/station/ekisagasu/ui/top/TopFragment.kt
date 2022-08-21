e4d6529e4512333e1455a01b260c04128a0e4d11
senda_k <s.kaoru509@gmail.com>
2022-08-21 20:56:13 +0900

    修正：layout ID

--- app/src/main/java/jp/seo/station/ekisagasu/ui/top/TopFragment.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/ui/top/TopFragment.kt
@@ -5,7 +5,11 @@ import android.animation.AnimatorSet
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.core.animation.addListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
