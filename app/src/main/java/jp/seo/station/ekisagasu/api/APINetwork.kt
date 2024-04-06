40c01c91ecfb6e2e3b687e37d81e810014d6b388
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-04-06 20:19:58 +0900

    fix: データURLの変更

--- app/src/main/java/jp/seo/station/ekisagasu/api/APINetwork.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/api/APINetwork.kt
@@ -1,64 +1,20 @@
 package jp.seo.station.ekisagasu.api
 
 import jp.seo.station.ekisagasu.model.DataLatestInfo
-import jp.seo.station.ekisagasu.model.StationData
-import kotlinx.serialization.ExperimentalSerializationApi
 import okhttp3.ResponseBody
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Streaming
-import retrofit2.http.Url
-import java.io.ByteArrayOutputStream
-import javax.inject.Inject
 
 /**
  * @author Seo-4d696b75
  * @version 2020/12/18.
  */
 interface APIClient {
-    @GET("latest_info.json")
+    @GET("station_database/latest_info.json")
     suspend fun getLatestInfo(): DataLatestInfo
 
-    @GET("out/main/data.json")
-    suspend fun getLatestData(): StationData
-
     @Streaming
-    @GET
-    suspend fun getData(@Url url: String): ResponseBody
-}
-
-interface DownloadClient {
-    /**
-     * 指定したURLからダウンロードする
-     *
-     * @param url URL文字列
-     * @param callback ダウンロード中に現在までに取得したbyte長を通知する
-     */
-    suspend operator fun invoke(url: String, callback: (Long) -> Unit): String
-}
-
-@ExperimentalSerializationApi
-class DownloadClientImpl @Inject constructor(
-    private val api: APIClient,
-) : DownloadClient {
-    // https://stackoverflow.com/questions/42118924/android-retrofit-download-progress
-    override suspend operator fun invoke(url: String, callback: (Long) -> Unit): String {
-        callback(0L)
-        api.getData(url).also { res ->
-            res.byteStream().use { inputStream ->
-                val outputStream = ByteArrayOutputStream()
-                var bytes = 0L
-                val buf = ByteArray(8192)
-                while (true) {
-                    val read = inputStream.read(buf)
-                    if (read < 0) {
-                        break
-                    }
-                    bytes += read
-                    outputStream.write(buf, 0, read)
-                    callback(bytes)
-                }
-                return String(outputStream.toByteArray())
-            }
-        }
-    }
+    @GET("station_database@{version}/out/main/json.zip")
+    suspend fun getLatestData(@Path("version") version: Long): ResponseBody
 }
--- app/src/main/java/jp/seo/station/ekisagasu/hilt/ApiModule.kt
+++ app/src/main/java/jp/seo/station/ekisagasu/hilt/ApiModule.kt
@@ -2,7 +2,6 @@ package jp.seo.station.ekisagasu.hilt
 
 import android.content.Context
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,8 +9,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import jp.seo.station.ekisagasu.R
 import jp.seo.station.ekisagasu.api.APIClient
-import jp.seo.station.ekisagasu.api.DownloadClient
-import jp.seo.station.ekisagasu.api.DownloadClientImpl
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType
@@ -61,15 +58,6 @@ object ApiModule {
     ): APIClient {
         return retrofit.create(APIClient::class.java)
     }
-}
-
-@ExperimentalSerializationApi
-@Module
-@InstallIn(SingletonComponent::class)
-interface DownloadModule {
-    @Singleton
-    @Binds
-    fun bindDownloadClient(impl: DownloadClientImpl): DownloadClient
 }
 
 @Module
