0af2d516847b25407b65216e6efbf7049107abaa
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-04-16 23:54:26 +0900

    feat: 権限リクエストのデータモデル・引数追加

---
+++ app/src/main/java/jp/seo/station/ekisagasu/ui/permission/PermissionRationale.kt
@@ -0,0 +1,19 @@
+package jp.seo.station.ekisagasu.ui.permission
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface PermissionRationale {
+    @SerialName("locationPermission")
+    data class LocationPermission(val showSystemRequestDialog: Boolean) : PermissionRationale
+
+    @SerialName("notificationPermission")
+    data class NotificationPermission(val showSystemRequestDialog: Boolean) : PermissionRationale
+
+    @SerialName("notificationChannel")
+    data object NotificationChannel : PermissionRationale
+
+    @SerialName("drawOverlay")
+    data object DrawOverlay : PermissionRationale
+}
