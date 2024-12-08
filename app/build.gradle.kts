0cdb5fa2610b6cd0e7570a59bf24035cea53b4d5
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-12-08 22:31:59 +0900

    feat: bump Android 15 targetSdkVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
     defaultConfig {
         applicationId = "jp.seo.station.ekisagasu"
         minSdk = 27
-        targetSdk = 33
-        compileSdk = 34
+        targetSdk = 35
+        compileSdk = 35
         versionCode = 209
         versionName = "2.3.1"
 
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 android {
     namespace = "com.seo4d696b75.android.ekisagasu.data"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 27
